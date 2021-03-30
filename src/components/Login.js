0a4eda271c2ca4a6107eb42cfa4f73ab0c717da8
marlenyg <marleny.garzon@wsu.edu>
2021-03-30 10:54:25 -0700

    LoginDraft1

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
 function Login() {
+    const username= userInputName('');
+    const password= userInputPassw('');
     return (
         <div className="Login">
             <h1>COVID-19 Vaccination Tracking Application</h1>
+            <div className ="Title"> 
+                <h2>Login</h2><br /><br />
+                <div>
+                Username<br />
+                <input type= "name" {...username} />
+                </div>
+                <div>
+                    Password<br />
+                    <input type="password" {...password} />
+                </div>
+            </div>
         </div>
     );
 }
