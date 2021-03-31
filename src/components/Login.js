87aae045730403122de26368cb84f42b35e2c24c
marlenyg <marleny.garzon@wsu.edu>
2021-03-30 19:42:15 -0700

    LoginDraft2

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 function Login() {
-    const username= userInputName('');
-    const password= userInputPassw('');
+    const username= userInput('');
+    const password= userInput('');
+    const [loading, setLoading]=useState(false);
+
+    const handleLogin=()=>{prompt.history}
     return (
         <div className="Login">
             <h1>COVID-19 Vaccination Tracking Application</h1>
@@ -16,9 +19,16 @@ function Login() {
                     Password<br />
                     <input type="password" {...password} />
                 </div>
+                <input type="button" value={loading ? 'Loading...' :'Login'} onClick={handleLogin} disabled={loading}/><br />
+
             </div>
         </div>
     );
+}
+const userInput= initialValue=>{const [value, setValue]= useState(initialValue);
+return{
+    value
+}
 }
 
 export default Login;
