1ed8e21e52c1430a686ba9a89b751f007fe30b2a
marlenyg <marleny.garzon@wsu.edu>
2021-03-31 11:25:20 -0700

    LogingDraft4

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,9 +7,8 @@ function Login() {
             <h1>COVID-19 Vaccination Tracking Application</h1>
             <div className ="Title"> 
                 <h2>Login</h2><br /><br />
-               
-              
             </div>
         </div>
     );}
+    <button onClick={Login}>Login</button>;
 export default Login;
