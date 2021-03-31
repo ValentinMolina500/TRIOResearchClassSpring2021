1130016a0bb1b5fdcd249b6ed86e79176268c739
marlenyg <marleny.garzon@wsu.edu>
2021-03-30 20:11:59 -0700

    logininRedo

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,13 +7,8 @@ function Login() {
             <h1>COVID-19 Vaccination Tracking Application</h1>
             <div className ="Title"> 
                 <h2>Login</h2><br /><br />
-                <div>
-              <form action="/action_page.php">
-                  <label for="username">UserName: </label>
-                  <input type="text" id="username" name="username"><br></br></input>
-                  <input type="login" value="Login"></input>
-              </form>
-                </div>
+               
+              
             </div>
         </div>
     );}
