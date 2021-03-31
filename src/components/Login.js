d9b4fb2f210dafaaf714e00f8ac1ac3c74358eca
marlenyg <marleny.garzon@wsu.edu>
2021-03-31 13:11:23 -0700

    LoginDraft5

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React,{useState} from 'react';
+
+
+function useInput(defaultValue){
+    const [value, setValue]= useState(defaultValue);
+
+    function onChange(e){
+        setValue(e.target.value);
+    }
+    return {
+        value,
+        onChange,
+    };
+}
 
 function Login() {
+    const inputProps = useInput();
 
     return (
         <div className="Login">
             <h1>COVID-19 Vaccination Tracking Application</h1>
             <div className ="Title"> 
                 <h2>Login</h2><br /><br />
+            </div>
+            <div>
+                <input {...inputProps}
+                placeholder="Username" />
             </div>
             <button onClick={Login}>Login</button>;
         </div>
