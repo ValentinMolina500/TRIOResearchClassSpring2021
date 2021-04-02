f179e7571fa17b932bb9ec70b650e03087dd744e
Valentin Molina <valentinmolina83@gmail.com>
2021-04-01 18:21:04 -0700

    Add dashboard page

---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+    return (
+        <div className="Dashboard">
+            <h1>Dashboard Page</h1>
+            <p>🚧 This is a WiP right now.</p>
+        </div>
+    );
+}
+
+export default Dashboard;
