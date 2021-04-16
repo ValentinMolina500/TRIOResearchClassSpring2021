99be349a15ea2dfdd73a028303c1a5d5c0016cbe
marlenyg <marleny.garzon@wsu.edu>
2021-04-15 19:03:18 -0700

    Poishing Signup page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,13 +1,58 @@
-import React from "react";
-
+/*import React from "react";*/
+import React, { useState } from "react";
+import { useHistory } from 'react-router-dom';
 import {
-	Heading
+	Input,
+	Box,
+    Heading,
+    Flex,
+    FormControl,
+    FormLabel,
+    Button
 } from "@chakra-ui/react";
 
-function Signup() {
+
+function SignUp() {
+    const[name, setName]=useState("");
+    const[last,setLast]=useState("");
+    const[email,setEmail]=useState("");
+
+    const history = useHistory();
+  const onButtonClick = () => {
+    history.push("/Login");
+  }
+
 	return (
-		<Heading>Hello World!</Heading>
+        <Flex width="100vw" height="100vh" align="center" justifyContent="center">
+          <Box p={2}>
+          <Box textAlign="center">
+            <Heading color="teal.200">Vaccination Tracking Sign Up Page</Heading>
+          </Box>
+          <Box my={4} textAlign="left">
+            <form>
+                  <FormControl>
+                  <FormLabel color="teal.600">First Name</FormLabel>
+                  <Input value={name} onChange={e=> setName(e.target.value)} placeholder="First Name"/>
+                  </FormControl>
+                  <FormControl>
+                  <FormLabel color="teal.600">Last Name</FormLabel>
+                  <Input value={last} onChange={e=> setLast(e.target.value)} placeholder="Last Name"/>
+                  </FormControl>
+                  <FormControl>
+                  <FormLabel color="teal.600">Email</FormLabel>
+                  <Input value={email} onChange={e=> setEmail(e.target.value)} placeholder="email@signup.com"/>
+                  </FormControl>
+                  <Button width="full" mt={4} type="submit">
+                submit
+                 </Button>
+              <Button onClick={onButtonClick} width="full" mt={4} type="submit" color="teal.600">
+              back to Login
+              </Button> 
+          </form>
+          </Box>
+          </Box>
+        </Flex>
 	);
 }
 
-export default Signup;
+export default SignUp;
